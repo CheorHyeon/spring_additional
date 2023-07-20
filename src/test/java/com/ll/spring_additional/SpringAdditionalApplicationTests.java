b723668a90940bf36a454f3877c8c6d2762a8f28
깃ChertHyeon <r4560798@naver.com>
2023-07-20 13:36:32 +0900

    setting

---
+++ src/test/java/com/ll/spring_additional/SpringAdditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.spring_additional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdditionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
