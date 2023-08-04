5bf0fb03e6b1c3b9ef3e3a4c5f3d30cf4510d56c
깃ChertHyeon <r4560798@naver.com>
2023-08-04 13:39:50 +0900

    NotProd 파일 질문 JDBC 배치쓰기로 변경

--- src/main/java/com/ll/spring_additional/base/initData/NotProd.java
+++ src/main/java/com/ll/spring_additional/base/initData/NotProd.java
@@ -1,5 +1,8 @@
 package com.ll.spring_additional.base.initData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +11,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ll.spring_additional.boundedContext.answer.entity.Answer;
+import com.ll.spring_additional.boundedContext.answer.repository.AnswerRepository;
 import com.ll.spring_additional.boundedContext.answer.service.AnswerService;
 import com.ll.spring_additional.boundedContext.question.entity.Question;
+import com.ll.spring_additional.boundedContext.question.repository.QuestionRepository;
 import com.ll.spring_additional.boundedContext.question.service.QuestionService;
 import com.ll.spring_additional.boundedContext.user.entity.SiteUser;
 import com.ll.spring_additional.boundedContext.user.service.UserService;
@@ -22,7 +27,9 @@ public class NotProd {
 		PasswordEncoder passwordEncoder,
 		QuestionService questionService,
 		UserService userService,
-		AnswerService answerService
+		AnswerService answerService,
+		QuestionRepository questionRepository,
+		AnswerRepository answerRepository
 	)
 	{
 		return new CommandLineRunner() {
@@ -30,17 +37,23 @@ public class NotProd {
 			@Transactional
 			public void run(String... args) throws Exception {
 
-				userService.create("admin","admin@test.com", "1234");
+				userService.create("admin","admin@test.com", passwordEncoder.encode("1234"));
 
-				SiteUser user1 = userService.create("user1", "user1@test.com", "1234");
-				SiteUser user2 = userService.create("user2", "user2@test.com", "1234");
-				SiteUser user3 = userService.create("puar12", "r4560798@naver.com", "1234");
+				SiteUser user1 = userService.create("user1", "user1@test.com", passwordEncoder.encode("1234"));
+				SiteUser user2 = userService.create("user2", "user2@test.com", passwordEncoder.encode("1234"));
+				SiteUser user3 = userService.create("puar12", "r4560798@naver.com", passwordEncoder.encode("1234"));
+
+				List<Question> list = new ArrayList<>();
 
 				for (int i = 1; i <= 300; i++) {
-						String subject = String.format("테스트 데이터입니다:[%03d]", i);
-						String content = "내용무";
-						questionService.create(subject, content, user1, 0);
-					}
+					Question tmp = new Question();
+					tmp.setSubject(String.format("테스트 데이터입니다:[%03d]", i));
+					tmp.setContent("내용무");
+					tmp.setAuthor(user2);
+					list.add(tmp);
+				}
+
+				questionRepository.saveAll(list);
 
 				Question question1 = questionService.create("질문입니닷", "질문이에요!", user2, 0);
 				Question question2 = questionService.create("질문입니닷22", "질문이에요!22", user2, 0);
@@ -48,6 +61,16 @@ public class NotProd {
 				Answer answer1 = answerService.create(question1, "답변1", user1);
 				Answer answer2 = answerService.create(question1, "답변2", user1);
 
+				List<Answer> answerList = new ArrayList<>();
+				for (int i = 1; i <= 300; i++) {
+					Answer tmp = new Answer();
+					tmp.setContent("테스트 답변%d".formatted(i));
+					tmp.setQuestion(question2);
+					tmp.setAuthor(user3);
+					answerList.add(tmp);
+				}
+
+				answerRepository.saveAll(answerList);
 				}
 			};
 	}
